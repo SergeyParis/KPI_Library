9990dd159a28f6c44ab1a515e8ae3e1fbd4b2ff3
Sergey Paris <bt1bt1990@gmail.com>
2018-11-07 12:08:21 +0200

    Library.Web - Books view finished

--- Library.Web/Controllers/HomeController.cs
+++ Library.Web/Controllers/HomeController.cs
@@ -13,15 +13,6 @@ namespace Library.Web.Controllers
         {
             ViewBag.Title = "Books";
 
-            Author author = DBManager.GetAuthorByName("Mark Twain");
-            Client client = DBManager.GetClientByName("Max Brazhnik");
-
-            DBManager.AddBook(new Book("The Birthday Boy", "1402742681", author));
-            DBManager.AddBook(new Book("The Spelling Bee", "140274269X", author));
-            DBManager.AddBook(new Book("The Adventures of Tom Sawyer #6: Tom's Treasure Hunt", "1402767544", author));
-            DBManager.AddBook(new Book("Romeo and Juliet", "1906230455", author));
-            DBManager.AddBook(new Book("Anna Karenina", "1911091042", author));
-
             IEnumerable<Book> list = DBManager.GetAllBooks();
 
             return View(list);
